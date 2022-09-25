1528ad81a28a3b775e1e19749ed1c67ef32cd5fc
fewkz <83943819+fewkz@users.noreply.github.com>
2022-09-25 10:54:51 +0000

    Refactor server

--- server/src/init.server.lua
+++ server/src/init.server.lua
@@ -1,49 +1,37 @@
--- CloneTrooper1019, 2020
--- Realism Server
-
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local CollectionService = game:GetService("CollectionService")
 
-local setLookAngles = Instance.new("RemoteEvent")
+local existing = ReplicatedStorage:FindFirstChild("SetLookAngles")
+local setLookAngles = existing or Instance.new("RemoteEvent")
+assert(
+	setLookAngles:IsA("RemoteEvent"),
+	"SetLookAngles found in ReplicatedStorage wasn't a RemoteEvent"
+)
 setLookAngles.Archivable = false
 setLookAngles.Name = "SetLookAngles"
 setLookAngles.Parent = ReplicatedStorage
 
-local function onReceiveLookAngles(player, pitch, yaw)
-	if typeof(pitch) ~= "number" or pitch ~= pitch then
-		return
-	end
-
-	if typeof(yaw) ~= "number" or yaw ~= yaw then
-		return
-	end
-
-	pitch = math.clamp(pitch, -1, 1)
-	yaw = math.clamp(yaw, -1, 1)
-
-	setLookAngles:FireAllClients(player, pitch, yaw)
+local function onCharacterAdded(character: Model)
+	local humanoid = character:WaitForChild("Humanoid", 4)
+	assert(humanoid, "Humanoid for " .. character.Name .. " couldn't be found")
+	CollectionService:AddTag(humanoid, "RealismHook")
 end
 
-local function onCharacterAdded(character)
-	local humanoid = character:WaitForChild("Humanoid", 10)
-
-	if humanoid and humanoid:IsA("Humanoid") then
-		CollectionService:AddTag(humanoid, "RealismHook")
-	end
-end
-
-local function onPlayerAdded(player)
+local function onPlayerAdded(player: Player)
+	player.CharacterAdded:Connect(onCharacterAdded)
 	if player.Character then
 		onCharacterAdded(player.Character)
 	end
-
-	player.CharacterAdded:Connect(onCharacterAdded)
-end
-
-for _, player in pairs(Players:GetPlayers()) do
-	onPlayerAdded(player)
 end
 
 Players.PlayerAdded:Connect(onPlayerAdded)
-setLookAngles.OnServerEvent:Connect(onReceiveLookAngles)
+for _, player in Players:GetPlayers() do
+	onPlayerAdded(player)
+end
+
+setLookAngles.OnServerEvent:Connect(function(player, pitch: unknown, yaw: unknown)
+	assert(typeof(pitch) == "number" and pitch == pitch, "Invalid pitch")
+	assert(typeof(yaw) == "number" and yaw == yaw, "Invalid yaw")
+	setLookAngles:FireAllClients(player, math.clamp(pitch, -1, 1), math.clamp(yaw, -1, 1))
+end)
